a9f49346f42a2d79699db3469043cea79a5ed9f4
summer2023 <happyxia1023@163.com>
2018-05-10 19:18:43 +0800

    write entity Company

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,55 @@
 package com.example.employee.entity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Company {
+    private long id;
+    private String companyName;
+    private Integer employeesNumber;
+
+    public Company() { }
+
+    public Company(long id, String companyName, Integer employeesNumber) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Company setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public Company setCompanyName(String companyName) {
+        this.companyName = companyName;
+        return this;
+    }
+
+    public Integer getemployeesNumber() {
+        return employeesNumber;
+    }
+
+    public Company setEmployeesNumber(Integer employeesNumber) {
+        this.employeesNumber = employeesNumber;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", this.id);
+        map.put("companyName", this.companyName);
+        map.put("employeesNumber", this.employeesNumber);
+        return map.toString();
+    }
 
 }
